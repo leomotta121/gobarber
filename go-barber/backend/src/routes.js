71299a440df54b64ec88d203b50663c573608e5f
Leonardo Motta <mail@leomotta.me>
2019-08-20 23:25:13 -0300

    Add database connection & test first create method

--- go-barber/backend/src/routes.js
+++ go-barber/backend/src/routes.js
@@ -1,5 +1,16 @@
-import { Router } from 'express';
+import express from 'express';
+import User from './app/models/User';
 
-const routes = new Router();
+const routes = express.Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Leo',
+    email: 'mail@leomotta.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
