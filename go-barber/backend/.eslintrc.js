0ce1850b88e999aeeb9a1a1de4ce56d8b899a5f1
Leonardo Motta <mail@leomotta.me>
2019-08-21 06:04:16 -0400

    Add UserController.js

--- go-barber/backend/.eslintrc.js
+++ go-barber/backend/.eslintrc.js
@@ -21,6 +21,6 @@ module.exports = {
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
     "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "nexr" }],
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
   },
 };
--- go-barber/backend/src/routes.js
+++ go-barber/backend/src/routes.js
@@ -1,16 +1,8 @@
 import express from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = express.Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Leo',
-    email: 'mail@leomotta.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
