485543fd032d960b036b50076787744991c08d6e
Leonardo Motta <mail@leomotta.me>
2019-10-17 17:09:59 -0300

    fix:fix enhancer typo

--- frontend/src/store/createStore.js
+++ frontend/src/store/createStore.js
@@ -1,13 +1,13 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 
 export default (reducers, middlewares) => {
-  const enhacer =
+  const enhancer =
     process.env.NODE_ENV === 'development'
       ? compose(
-          console.tron.createEnhacer(),
+          console.tron.createEnhancer(),
           applyMiddleware(...middlewares)
         )
       : applyMiddleware(...middlewares);
 
-  return createStore(reducers, enhacer);
+  return createStore(reducers, enhancer);
 };
