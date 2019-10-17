b1abe92927f1ead0c7b16c9f1dbcff7722cab3a4
Leonardo Motta <mail@leomotta.me>
2019-10-17 12:44:33 -0300

    feature:add signup and signin pages

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Email is not valid.')
+    .required('Email is required.'),
+  password: Yup.string().required('Password is required.'),
+});
 
 export default function SignIn() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber logo" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
+
+        <button type="submit">Send</button>
+
+        <Link to="/signup">Not registered?</Link>
+      </Form>
+    </>
+  );
 }
--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber logo" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+        <Input name="password" type="password" placeholder="Your password" />
+
+        <button type="submit">Send</button>
+
+        <Link to="/">Already registered?</Link>
+      </Form>
+    </>
+  );
 }
